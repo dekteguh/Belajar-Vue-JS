9c10281d74285dd0ba511f72f13acf2ea053b3e6
Eko Teguh Widodo <dekteguh@gmail.com>
2017-05-12 17:14:36 +0700

    menambah project untuk 04_Data_Binding

---
+++ 04_Data_Binding/app.js
@@ -0,0 +1,12 @@
+new Vue({
+    el: '#vue-app',
+      data: {
+        name: 'Eko Teguh Widodo',
+        job: 'Software Engineer at BPS',
+        website: 'http://ekoteguh.my.id',
+        websiteTag: '<a href="http://ekoteguh.my.id">My Blog</a>'
+     },
+    methods: {
+
+    }
+});
